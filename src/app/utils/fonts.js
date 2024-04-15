c1bdac15de96b62a0a1150f1fc937b6ffb5cd358
zachary-davies <zachary.davies.dev@gmail.com>
2024-04-15 10:58:31 -0400

    Removing some default settings and stylings, add TypeWriter library, starting to add details and header

---
+++ src/app/utils/fonts.js
@@ -0,0 +1,11 @@
+import { Sometype_Mono, Inter } from "next/font/google";
+
+export const someTypeFont = Sometype_Mono({
+  subsets: ["latin"],
+  variable: "--font-someType",
+});
+
+export const interFont = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
