54400fe1429bfab2a05c152397557132d4063f70
zachary-davies <zachary.davies.dev@gmail.com>
2024-05-01 10:19:32 -0400

    Adding a basepath to see if this fixes the resume problem without causing other problems

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //basePath: "/portfolio",
+  basePath: "/portfolio",
   output: "export",
   reactStrictMode: true,
   images: { unoptimized: true },
